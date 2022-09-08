af4e7a347fca2e0424e92ef5a6af42aa64d3e913
unknown <aquinasmoriasi@gmail.com>
2022-09-09 00:09:20 +0300

    add mapping function for fetched data

--- src/redux/cryptocurrencies/cryptos.js
+++ src/redux/cryptocurrencies/cryptos.js
@@ -7,9 +7,44 @@ export const fetchCryptos = createAsyncThunk(
   GET_CRYPTOS,
   async () => {
     const data = await fetch('https://api.coingecko.com/api/v3/coins/');
-    const response = await data.json();
-    const filteredResponse = response.slice(0, 20);
-    return filteredResponse;
+    const json = await data.json();
+    const response = json.slice(0, 20);
+    const arr = [];
+    const keys = Object.keys(response);
+    keys.map((key) => arr.push({
+      id: response[key].id,
+      symbol: response[key].symbol,
+      name: response[key].name,
+      image: {
+        large: response[key].image.large,
+      },
+      market_data: {
+        current_price: {
+          btc: response[key].market_data.current_price.btc,
+          usd: response[key].market_data.current_price.usd,
+        },
+        market_cap: {
+          btc: response[key].market_data.market_cap.btc,
+          usd: response[key].market_data.market_cap.usd,
+        },
+        market_cap_rank: response[key].market_data.market_cap_rank,
+        total_volume: {
+          btc: response[key].market_data.total_volume.btc,
+          usd: response[key].market_data.total_volume.usd,
+        },
+        high_24h: {
+          btc: response[key].market_data.high_24h.btc,
+          usd: response[key].market_data.high_24h.usd,
+        },
+        low_24h: {
+          btc: response[key].market_data.low_24h.btc,
+          usd: response[key].market_data.low_24h.usd,
+        },
+        price_change_percentage_24h:
+            response[key].market_data.price_change_percentage_24h,
+      },
+    }));
+    return arr;
   },
 );
 
